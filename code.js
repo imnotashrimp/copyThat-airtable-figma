defbc28c9ea03d19ff0fb001d92c531916cc0c4d
Shalom Boroda <imnotashrimp@gmail.com>
2019-11-11 21:42:02 +0200

    build plugin => ui communication

--- code.js
+++ code.js
@@ -1,6 +1,3 @@
-// This file holds the main code for the plugins. It has access to the *document*.
-// You can access browser APIs in the <script> tag inside "ui.html" which has a
-// full browser enviroment (see documentation).
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
@@ -10,82 +7,93 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
-// This shows the HTML page in "ui.html".
+const getData = (key) => {
+    return figma.root.getPluginData(key) || undefined;
+};
+const setData = (key, value) => {
+    figma.root.setPluginData(key, value);
+    return;
+};
+const airtableKeys = {
+    apiKey: 'apiKey',
+    baseId: 'baseId',
+    primaryKey: 'primaryKey',
+    viewName: 'viewName'
+};
+var airtable = {
+    config: {
+        apiKey: getData(airtableKeys.apiKey),
+        baseId: getData(airtableKeys.baseId),
+        primaryKey: getData(airtableKeys.primaryKey),
+        viewName: getData(airtableKeys.viewName)
+    },
+    set: {
+        setApiKey: (apiKey) => {
+            setData(airtableKeys.apiKey, apiKey);
+            airtable.config.apiKey = getData(airtableKeys.apiKey);
+        },
+        setBaseId: (baseId) => {
+            setData(airtableKeys.baseId, baseId);
+            airtable.config.apiKey = getData(airtableKeys.baseId);
+        },
+        setPrimaryKey: (primaryKey) => {
+            setData(airtableKeys.primaryKey, primaryKey);
+            airtable.config.apiKey = getData(airtableKeys.primaryKey);
+        },
+        setViewName: (viewName) => {
+            setData(airtableKeys.viewName, viewName);
+            airtable.config.apiKey = getData(airtableKeys.viewName);
+        }
+    }
+};
 figma.showUI(__html__);
+figma.ui.postMessage(airtable.config);
 const variablePattern = /^\{{2}.+\}{2}$/m;
-function isVariable(string) {
+function isVariable(testString) {
     // Test if input string is a variable
-    return variablePattern.test(string);
+    return variablePattern.test(testString);
 }
 // Calls to "parent.postMessage" from within the HTML page will trigger this
 // callback. The callback will be passed the "pluginMessage" property of the
 // posted message.
-/* figma.ui.onmessage = msg => {
-  // One way of distinguishing between different types of messages sent from
-  // your HTML page is to use an object with a "type" property like this.
-  if (msg.type === 'create-rectangles') {
-    const nodes: SceneNode[] = [];
-    for (let i = 0; i < msg.count; i++) {
-      const rect = figma.createRectangle();
-      rect.x = i * 150;
-      rect.fills = [{type: 'SOLID', color: {r: 1, g: 0.5, b: 0}}];
-      figma.currentPage.appendChild(rect);
-      nodes.push(rect);
+figma.ui.onmessage = (msg) => __awaiter(this, void 0, void 0, function* () {
+    if (msg.type === 'save-airtable-config') {
+        console.log('Message received from Figma UI: ', msg.keys);
     }
-    figma.currentPage.selection = nodes;
-    figma.viewport.scrollAndZoomIntoView(nodes);
-  }
-
-  // Make sure to close the plugin when you're done. Otherwise the plugin will
-  // keep running, which shows the cancel button at the bottom of the screen.
-  figma.closePlugin();
-};
-*/
-for (const node of figma.currentPage.selection) {
-    // If it's not a variable or a text, move along.
-    if (node.type !== 'TEXT') {
-        break;
-    }
-    if (!isVariable(node.characters) && !isVariable(node.name)) {
-        console.log(node.name + ' not a variable. Moving along.');
-        break;
-    }
-    airtableTest();
-    node.autoRename = false;
-    //  TODO confirmation dialog
-    if (isVariable(node.name) === true && node.name !== node.characters)
-        updateText(node, node.name);
-}
-;
-figma.closePlugin();
-function updateText(node, text) {
-    return __awaiter(this, void 0, void 0, function* () {
-        if (node.hasMissingFont) {
-            // TODO handle missing fonts
-            console.log('There are missing fonts. Not updating ' + node.name + '.');
-        }
-        else {
-            // Figma requires this bit when replacing text
-            yield figma.loadFontAsync(node.fontName);
-            // Replace the text in the node
-            node.characters = text;
-        }
-    });
-}
-function airtableTest() {
-    return __awaiter(this, void 0, void 0, function* () {
-        console.log('Starting airtableTest function');
-        // var request = new XMLHttpRequest();
-        // request.withCredentials = true;
-        // request.open("GET", "https://api.airtable.com/v0/appob2x3Fc4an3Uoq/logzio-ui?maxRecords=1&fields=key,theCopy&filterByFormula=AND%28key=%27alerts.triggerIfStep.stepTitle.text%27%29");
-        // request.setRequestHeader("Authorization", "Bearer keyqUqOMoRi6L9ctP");
-        // request.responseType = 'text'
-        // request.onload = () => {
-        //   // window.parent.postMessage({pluginMessage: request.response}, '*')
-        //   console.log('onload hit');
-        // };
-        // request.send();
-        // console.log(request.response);
-        console.log('airtableTest function done');
-    });
-}
+    figma.closePlugin();
+});
+// function changeTextToTag () {
+//   for (const node of figma.currentPage.selection) {
+//     // If it's not a variable or a text, move along.
+//     if (node.type !== 'TEXT') { break; }
+//     if (!isVariable(node.characters) && !isVariable(node.name)) {
+//       console.log(node.name + ' not a variable. Moving along.');
+//       break;
+//     }
+//     node.autoRename = false;
+//     //  TODO confirmation dialog
+//     if (isVariable(node.name) === true && node.name !== node.characters)
+//       updateText(node, node.name);
+//   };
+//   figma.closePlugin();
+// }
+// async function updateText(node, text) {
+//   if (node.hasMissingFont) {
+//     // TODO handle missing fonts
+//     console.log('There are missing fonts. Not updating ' + node.name + '.')
+//   } else {
+//     // Figma requires this bit when replacing text
+//     await figma.loadFontAsync(node.fontName);
+//     // Replace the text in the node
+//     node.characters = text;
+//   }
+// }
+// flow - save Airtable info
+// need - API key, base ID, view name, primary key field
+// flow - update all strings
+// under the hood:
+//    1. get airtable data
+//    2. transform -
+//        a. traverse to 'records'
+//        b. iterate thru array, replace the "id" field with "key"
+//    3. 
--- code.ts
+++ code.ts
@@ -1,97 +1,118 @@
-// This file holds the main code for the plugins. It has access to the *document*.
-// You can access browser APIs in the <script> tag inside "ui.html" which has a
-// full browser enviroment (see documentation).
+const getData = (key: string) => {
+  return figma.root.getPluginData(key) || undefined;
+}
 
-// This shows the HTML page in "ui.html".
+const setData = (key: string, value: string) => {
+  figma.root.setPluginData(key, value);
+  return;
+}
+
+const airtableKeys = {
+  apiKey: 'apiKey',
+  baseId: 'baseId',
+  primaryKey: 'primaryKey',
+  viewName: 'viewName'
+};
+
+
+var airtable = {
+  config: {
+    apiKey: getData(airtableKeys.apiKey),
+    baseId: getData(airtableKeys.baseId),
+    primaryKey: getData(airtableKeys.primaryKey),
+    viewName: getData(airtableKeys.viewName)
+  },
+
+  set: {
+    setApiKey: (apiKey: string) => {
+      setData(airtableKeys.apiKey, apiKey);
+      airtable.config.apiKey = getData(airtableKeys.apiKey);
+    },
+
+    setBaseId: (baseId: string) => {
+      setData(airtableKeys.baseId, baseId);
+      airtable.config.apiKey = getData(airtableKeys.baseId);
+    },
+
+    setPrimaryKey: (primaryKey: string) => {
+      setData(airtableKeys.primaryKey, primaryKey);
+      airtable.config.apiKey = getData(airtableKeys.primaryKey);
+    },
+
+    setViewName: (viewName: string) => {
+      setData(airtableKeys.viewName, viewName);
+      airtable.config.apiKey = getData(airtableKeys.viewName);
+    }
+  }
+}
+
 figma.showUI(__html__);
+figma.ui.postMessage(airtable.config)
 
 const variablePattern = /^\{{2}.+\}{2}$/m;
-
-function isVariable(string) {
+function isVariable(testString: string) {
   // Test if input string is a variable
-  return variablePattern.test(string);
+  return variablePattern.test(testString);
 }
 
 // Calls to "parent.postMessage" from within the HTML page will trigger this
 // callback. The callback will be passed the "pluginMessage" property of the
 // posted message.
-/* figma.ui.onmessage = msg => {
-  // One way of distinguishing between different types of messages sent from
-  // your HTML page is to use an object with a "type" property like this.
-  if (msg.type === 'create-rectangles') {
-    const nodes: SceneNode[] = [];
-    for (let i = 0; i < msg.count; i++) {
-      const rect = figma.createRectangle();
-      rect.x = i * 150;
-      rect.fills = [{type: 'SOLID', color: {r: 1, g: 0.5, b: 0}}];
-      figma.currentPage.appendChild(rect);
-      nodes.push(rect);
-    }
-    figma.currentPage.selection = nodes;
-    figma.viewport.scrollAndZoomIntoView(nodes);
+figma.ui.onmessage = async (msg) => {
+  if (msg.type === 'save-airtable-config') {
+    console.log('Message received from Figma UI: ', msg.keys)
   }
 
-  // Make sure to close the plugin when you're done. Otherwise the plugin will
-  // keep running, which shows the cancel button at the bottom of the screen.
   figma.closePlugin();
-};
-*/
-
-for (const node of figma.currentPage.selection) {
-
-  // If it's not a variable or a text, move along.
-  if (node.type !== 'TEXT') { break; }
-  if (!isVariable(node.characters) && !isVariable(node.name)) {
-    console.log(node.name + ' not a variable. Moving along.');
-    break;
-   }
-
-  airtableTest();
-
-  node.autoRename = false;
-
-  //  TODO confirmation dialog
-
-  if (isVariable(node.name) === true && node.name !== node.characters)
-    updateText(node, node.name);
-
-};
-figma.closePlugin();
-
-async function updateText(node, text) {
-  if (node.hasMissingFont) {
-
-    // TODO handle missing fonts
-    console.log('There are missing fonts. Not updating ' + node.name + '.')
-
-  } else {
-
-    // Figma requires this bit when replacing text
-    await figma.loadFontAsync(node.fontName);
-
-    // Replace the text in the node
-    node.characters = text;
-
-  }
 }
 
-async function airtableTest() {
-  console.log('Starting airtableTest function')
+// function changeTextToTag () {
+//   for (const node of figma.currentPage.selection) {
 
-  // var request = new XMLHttpRequest();
-  // request.withCredentials = true;
+//     // If it's not a variable or a text, move along.
+//     if (node.type !== 'TEXT') { break; }
+//     if (!isVariable(node.characters) && !isVariable(node.name)) {
+//       console.log(node.name + ' not a variable. Moving along.');
+//       break;
+//     }
 
-  // request.open("GET", "https://api.airtable.com/v0/appob2x3Fc4an3Uoq/logzio-ui?maxRecords=1&fields=key,theCopy&filterByFormula=AND%28key=%27alerts.triggerIfStep.stepTitle.text%27%29");
-  // request.setRequestHeader("Authorization", "Bearer keyqUqOMoRi6L9ctP");
-  // request.responseType = 'text'
+//     node.autoRename = false;
 
-  // request.onload = () => {
-  //   // window.parent.postMessage({pluginMessage: request.response}, '*')
-  //   console.log('onload hit');
-  // };
+//     //  TODO confirmation dialog
 
-  // request.send();
+//     if (isVariable(node.name) === true && node.name !== node.characters)
+//       updateText(node, node.name);
 
-  // console.log(request.response);
-  console.log('airtableTest function done')
-}
+//   };
+//   figma.closePlugin();
+// }
+
+// async function updateText(node, text) {
+//   if (node.hasMissingFont) {
+
+//     // TODO handle missing fonts
+//     console.log('There are missing fonts. Not updating ' + node.name + '.')
+
+//   } else {
+
+//     // Figma requires this bit when replacing text
+//     await figma.loadFontAsync(node.fontName);
+
+//     // Replace the text in the node
+//     node.characters = text;
+
+//   }
+// }
+
+
+
+// flow - save Airtable info
+// need - API key, base ID, view name, primary key field
+
+// flow - update all strings
+// under the hood:
+//    1. get airtable data
+//    2. transform -
+//        a. traverse to 'records'
+//        b. iterate thru array, replace the "id" field with "key"
+//    3. 
