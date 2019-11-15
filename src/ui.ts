c723eaa252343570f6016da580ab5ade8c43c0db
Shalom Boroda <imnotashrimp@gmail.com>
2019-11-15 11:54:21 +0200

    refactor airtable pagination

--- src/ui.ts
+++ src/ui.ts
@@ -28,42 +28,62 @@ onmessage = (event) => {
     console.log('sync called');
     const airtableConfig = msg.airtableConfig;
 
-    const getAirtableStrings = async (airtableConfig) => {
-      var request = new XMLHttpRequest()
-      const apiKey = airtableConfig.apiKey;
-      const baseId = airtableConfig.baseId;
-      const tableName = airtableConfig.tableName;
-      const primaryKeyField = airtableConfig.primaryKeyField;
-      const theCopyField = airtableConfig.theCopyField;
+    var allStrings = {};
+    const addStrings = (records) => {
+      // Parses Airtable response fields, generates new object, and appends to
+      // allStrings array
+      records.forEach((record) => {
+        var key = record.fields[primaryKeyField];
+        var value = record.fields[theCopyField];
 
-      const endpointUrl =
-        'https://api.airtable.com/v0/'
-        + baseId
-        + '/'
-        + tableName
-        + '?api_key='
-        + apiKey
-        + '&fields='
-        + primaryKeyField
-        + '&fields='
-        + theCopyField
-        ;
-
-      request.open('GET', endpointUrl);
-
-      request.responseType = 'text'
-      request.onload = () => {
-        const msg = {
-          type: 'sync-airtable-strings',
-          response: request.response
-        }
-        window.parent.postMessage({pluginMessage: msg}, '*')
-      };
-      request.send();
-
+        // Update allStrings object, to be sent to the plugin
+        allStrings = Object.assign(allStrings, {[key]: value});
+      })
     }
 
-    getAirtableStrings(airtableConfig);
+    const apiKey = airtableConfig.apiKey;
+    const baseId = airtableConfig.baseId;
+    const tableName = airtableConfig.tableName;
+    const primaryKeyField = airtableConfig.primaryKeyField;
+    const theCopyField = airtableConfig.theCopyField;
+    const apiBaseUrl = 'https://api.airtable.com/v0/'
+      + baseId
+      + '/'
+      + tableName
+      + '?api_key='
+      + apiKey;
+
+    const pageToFetch = (page: 'first' | 'next', offset?: string) => {
+      switch(page) {
+        case 'first':
+          return apiBaseUrl
+            + '&fields=' + primaryKeyField
+            + '&fields=' + theCopyField
+            ;
+
+        case 'next':
+          return apiBaseUrl + '&offset=' + offset;
+      }
+    }
+
+    const getResults = async (page: 'first' | 'next', offset?: string) => {
+      var url = pageToFetch(page, offset);
+      var records: string;
+      var response = await makeAirtableCall(url);
+
+      // Amend the allStrings object, to be passed back to the plugin
+      if (typeof(response) === 'string') {
+        records = JSON.parse(response).records;
+        offset = JSON.parse(response).offset;
+        addStrings(records);
+      }
+
+      // Get next page if it's there
+      if (offset) getResults('next', offset);
+    }
+
+    getResults('first');
+    // console.log(allStrings); // debug
 
   }
 }
@@ -83,4 +103,21 @@ document.getElementById('save').onclick = () => {
 
 document.getElementById('cancel').onclick = () => {
   parent.postMessage({ pluginMessage: { type: 'cancel' } }, '*');
+}
+
+const makeAirtableCall = (url: string) => {
+  return new Promise( (resolve, reject) => {
+    var request = new XMLHttpRequest()
+    request.open('GET', url);
+
+    request.responseType = 'text';
+    try {
+      request.send();
+    } catch (error) {
+      return reject(error);
+    }
+    request.onload = () => {
+      return resolve (request.response);
+    }
+  })
 }
