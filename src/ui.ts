5a0b5f6d69b3db4de3691c3fa4f0b5aca92e240a
Shalom Boroda <imnotashrimp@gmail.com>
2019-12-26 16:42:22 +0200

    semicolons out

--- src/ui.ts
+++ src/ui.ts
@@ -44,45 +44,45 @@ const validationMsg = {
 }
 
 const getFieldValue = (data: string) => {
-  if (!data) return '';
-  return data;
+  if (!data) return ''
+  return data
 }
 
 onmessage = (event) => {
-  const msg = event.data.pluginMessage;
-  const type = msg.type;
+  const msg = event.data.pluginMessage
+  const type = msg.type
 
   if (type === 'config') {
-    const data = msg.airtableConfig;
-    // console.log("Received from plugin: ", data); // debug
+    const data = msg.airtableConfig
+    // console.log("Received from plugin: ", data) // debug
 
-    formFields.apiKey['value'] = getFieldValue(data.apiKey);
-    formFields.baseId['value'] = getFieldValue(data.baseId);
-    formFields.tableName['value'] = getFieldValue(data.tableName);
-    formFields.primaryKeyField['value'] = getFieldValue(data.primaryKeyField);
-    formFields.theCopyField['value'] = getFieldValue(data.theCopyField);
-    formFields.lastUpdatedDate['innerHTML'] = getFieldValue(data.lastUpdatedDate);
+    formFields.apiKey['value'] = getFieldValue(data.apiKey)
+    formFields.baseId['value'] = getFieldValue(data.baseId)
+    formFields.tableName['value'] = getFieldValue(data.tableName)
+    formFields.primaryKeyField['value'] = getFieldValue(data.primaryKeyField)
+    formFields.theCopyField['value'] = getFieldValue(data.theCopyField)
+    formFields.lastUpdatedDate['innerHTML'] = getFieldValue(data.lastUpdatedDate)
   }
 
   if (type === 'sync') {
-    console.log('sync called');
+    console.log('sync called')
     const getAllStrings = async () => {
 
       let allStringsArr = await getStringsFromAirtable(msg.airtableConfig, msg.varNames)
 
       // Convert allStringsArr to an object
-      var allStringsObj = Object.assign({}, ...allStringsArr);
-      // console.log(allStringsObj); // debug
+      var allStringsObj = Object.assign({}, ...allStringsArr)
+      // console.log(allStringsObj) // debug
 
       var msgToPlugin = {
         type: 'sync-airtable-strings',
         strings: allStringsObj
       }
 
-      // console.log(msgToPlugin); // debug
-      parent.postMessage({ pluginMessage: msgToPlugin }, '*');
+      // console.log(msgToPlugin) // debug
+      parent.postMessage({ pluginMessage: msgToPlugin }, '*')
     }
-    getAllStrings();
+    getAllStrings()
   }
 }
 
@@ -93,62 +93,62 @@ document.getElementById('save').onclick = () => {
     , tableName: getFieldValue(formFields.tableName['value'])
     , primaryKeyField: getFieldValue(formFields.primaryKeyField['value'])
     , theCopyField: getFieldValue(formFields.theCopyField['value'])
-  };
+  }
 
   // Validate
-  var isValid = validateForm(keys);
+  var isValid = validateForm(keys)
   // Prevent saving if form isn't valid
-  if (isValid === false) return;
+  if (isValid === false) return
 
-  console.log('Sending to plugin: ', keys);
-  parent.postMessage({ pluginMessage: { type: 'save-airtable-config', keys } }, '*');
+  console.log('Sending to plugin: ', keys)
+  parent.postMessage({ pluginMessage: { type: 'save-airtable-config', keys } }, '*')
 }
 
 document.getElementById('cancel').onclick = () => {
-  parent.postMessage({ pluginMessage: { type: 'cancel' } }, '*');
+  parent.postMessage({ pluginMessage: { type: 'cancel' } }, '*')
 }
 
 function validateForm (keys) {
-  var formIsValid = true;
-  const isRequired = 'This is required';
-  const mainMsg = 'Please fix the issues in red and try again.';
+  var formIsValid = true
+  const isRequired = 'This is required'
+  const mainMsg = 'Please fix the issues in red and try again.'
 
   if (keys.apiKey === '') {
-    formIsValid = false;
-    validationMsg.apiKey.innerText = isRequired;
+    formIsValid = false
+    validationMsg.apiKey.innerText = isRequired
   } else {
-    validationMsg.apiKey.innerText = '';
+    validationMsg.apiKey.innerText = ''
   }
 
   if (keys.baseId === '') {
-    formIsValid = false;
-    validationMsg.baseId.innerText = isRequired;
+    formIsValid = false
+    validationMsg.baseId.innerText = isRequired
   } else {
-    validationMsg.baseId.innerText = '';
+    validationMsg.baseId.innerText = ''
   }
 
   if (keys.tableName === '') {
-    formIsValid = false;
-    validationMsg.tableName.innerText = isRequired;
+    formIsValid = false
+    validationMsg.tableName.innerText = isRequired
   } else {
-    validationMsg.tableName.innerText = '';
+    validationMsg.tableName.innerText = ''
   }
 
   if (keys.primaryKeyField === '') {
-    formIsValid = false;
-    validationMsg.primaryKeyField.innerText = isRequired;
+    formIsValid = false
+    validationMsg.primaryKeyField.innerText = isRequired
   } else {
-    validationMsg.primaryKeyField.innerText = '';
+    validationMsg.primaryKeyField.innerText = ''
   }
 
   if (keys.theCopyField === '') {
-    formIsValid = false;
-    validationMsg.theCopyField.innerText = isRequired;
+    formIsValid = false
+    validationMsg.theCopyField.innerText = isRequired
   } else {
-    validationMsg.theCopyField.innerText = '';
+    validationMsg.theCopyField.innerText = ''
   }
 
   if (formIsValid === false)
-    validationMsg.mainValidationMsg.innerText = mainMsg;
-  return formIsValid;
+    validationMsg.mainValidationMsg.innerText = mainMsg
+  return formIsValid
 }
